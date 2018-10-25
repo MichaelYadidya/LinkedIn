280d2f0891313b25b2c79bf6b8bfe1c1fce53260
Michael <michaelyadidya@gmail.com>
2018-10-25 20:47:08 +0530

    Initial commit

---
+++ paramaters.py
@@ -0,0 +1,5 @@
+search_query = 'site:linkedin.com/in/ AND "python developer" AND "London"' # Search
+file_name = 'results_file.csv'
+
+linkedin_username = 'test@gmail.com' # Your linkedin email goes here !
+linkedin_password = ''  # YOur Linkedin password goes here !
